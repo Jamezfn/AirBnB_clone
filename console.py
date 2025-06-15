d356bccf3751b22257cdbd4d15464c4a4ce6507a
Jamezfn <jamesmakachia19@gmail.com>
2025-06-15 12:01:34 +0300

    Implement command methods for create, show, destroy, all, and update in the console

--- console.py
+++ console.py
@@ -3,6 +3,13 @@
 Command interpreter for AirBnB clone
 """
 import cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
+
+classes = {
+        'BaseModel': BaseModel,
+        }
 
 class HBNBCommand(cmd.Cmd):
     """HBNB command interpreter"""
@@ -23,6 +30,133 @@ class HBNBCommand(cmd.Cmd):
     def help_quit(self, args):
         """Quit commant to exit the program"""
         print("Quit command to exit the program")
+
+    def do_create(self, arg):
+        """create <class name>: create new instance, save, and print id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        cls_name = args[0]
+        if cls_name not in classes:
+            print("** class doesn't exist **")
+            return
+        obj = classes[cls_name]()
+        obj.save()
+        print(obj.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        cls_name = args[0]
+        if cls_name not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = f"{cls_name}.{obj_id}"
+        objs = storage.all()
+        if key not in objs:
+            print("** no instance found **")
+            return
+        print(objs[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        cls_name = args[0]
+        if cls_name not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = f"{cls_name}.{obj_id}"
+        objs = storage.all()
+        if key not in objs:
+            print("** no instance found **")
+            return
+        del objs[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all instances based or not on the class names"""
+        args = shlex.split(arg)
+        objs = storage.all()
+        out_list = []
+        if len(args) == 0:
+            for obj in objs.values():
+                out_list.append(str(obj))
+        else:
+            cls_name = args[0]
+            if cls_name not in classes:
+                print("** class doesn't exist **")
+                return
+            for key, obj in objs.items():
+                if key.startswith(f"{cls_name}"):
+                    out_list.append(str(obj))
+
+        print(out_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        cls_name = args[0]
+        if cls_name not in classes:
+            print("** class doesn't exist **")
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = f"{cls_name}.{obj_id}"
+        objs = storage.all()
+        if key not in objs:
+            print("** no instance found **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        attr = args[2]
+        if len(args) == 3:
+            print("** value missing **")
+            return
+        attr_value = args[3]
+        obj = objs[key]
+        if hasattr(obj, attr_value):
+            current = getattr(obj, attr_value)
+            cast_value = None
+            try:
+                if isinstance(current, int):
+                    cast_value = int(attr_value)
+                elif isinstance(current, float):
+                    cast_value = float(attr_value)
+                else:
+                    cast_value = attr_value
+            except Exception:
+                cast_value = attr_value
+        else:
+            if attr_value.isdigit():
+                cast_value = int(attr_value)
+            else:
+                try:
+                    cast_value = float(attr_value)
+                except ValueError:
+                    cast_value = attr_value
+        setattr(obj, attr, cast_value)
+        obj.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
