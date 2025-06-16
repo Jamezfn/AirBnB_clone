f7ff955fb8e4444141f2826980abc4450ad55e8a
Jamezfn <jamesmakachia19@gmail.com>
2025-06-16 08:51:16 +0300

    Add State model to console and file storage

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
+from models.state import State
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
@@ -16,6 +17,7 @@ classes = {
         'BaseModel': BaseModel,
         'User': User,
         'City': City,
+        'State': State,
         'Amenity': Amenity,
         'Place': Place,
         'Review': Review
